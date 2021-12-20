bf8a3caf6d5f3b8a207ba6394e0cc2f6aa531e01
issa0133 <19950625issa@gmail.com>
2021-12-20 07:17:40 +0000

    [Finish]4-5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6c3818f6ad16de6fae35b6fe9508d58f12f4ac44e66019af625a401f230a9d5fb9764d7b6b816c2e17bffe05a0d123f1f024210a8af856b85915dfcb2ae8d7e0'
