0e54c6eafc4523552777aa892d797092d510d3a8
issa0133 <19950625issa@gmail.com>
2021-12-21 02:58:16 +0000

    [Finish]4-9

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,8 +7,12 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id #現在ログイン中のユーザー情報を取得する。
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
+    
   end
   
   def index
